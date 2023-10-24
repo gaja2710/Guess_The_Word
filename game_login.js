76b4baf37068754df92511392c23d65dfc5fab97
gaja2710 <90480594+gaja2710@users.noreply.github.com>
2023-10-24 18:05:30 +1000

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,11 @@
+function addUser() {
+    player_1_name = document.getElementById("player_1").value;
+    player_2_name = document.getElementById("player_2").value;
+
+    localStorage.setItem("player_1", player_1_name);
+    localStorage.setItem("player_2", player_2_name);
+
+    console.log(player_1_name, player_2_name);
+
+    window.location = "game_page.html";
+}
